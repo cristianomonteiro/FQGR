253609397f038d5ef59ae8a4fb155458bf4eb220
cristianomartinsm <cristianomartinsm@gmail.com>
2020-09-02 22:29:00 -0300

    Draft of question 1 and almost complete draft for question 2

--- Lista1/Exercicio1.py
+++ Lista1/Exercicio1.py
@@ -1,1 +1,38 @@
 import numpy as np
+
+preco1Acao = 100
+precoOpcao = 1.6
+pExercicio = 102
+
+#Cenário R$105
+preco2Acao = 105
+patrimonio105 = preco1Acao - preco2Acao - precoOpcao + (preco2Acao - pExercicio)
+print(patrimonio105)
+
+#Cenário R$95
+preco2Acao = 95
+patrimonio95 = preco1Acao - preco2Acao - precoOpcao
+print(patrimonio95)
+
+#Calculando o Delta
+#0 + PrecoOpcao + 100*Delta - 105*Delta + (105 - 102) = 0 + PrecoOpcao + 100*Delta - 95*Delta
+#PrecoOpcao - 5*Delta + 3 = 5*Delta + PrecoOpcao
+#-10*Delta = -3
+#Delta = 0.3
+
+#Calculando o preço justo para a opção
+#0 + PrecoOpcao = 100*Delta - 105*Delta + (105 - 102)
+#0 + PrecoOpcao = 100*0,3 - 105*0,3 + 3
+#PrecoOpcao = 30 - 31,5 + 3 = 1,5
+
+#Calculando o Delta considerando taxa livre de risco de 1%
+#0*1,01 + PrecoOpcao + 100*Delta*1,01 - 105*Delta + (105 - 102) = 0*1,01 + PrecoOpcao + 100*Delta*1,01 - 95*Delta
+#PrecoOpcao + 101*Delta - 105*Delta + 3 = PrecoOpcao + 101*Delta - 95*Delta
+#PrecoOpcao - 4*Delta + 3 = PrecoOpcao + 6*Delta
+#-10*Delta = -3
+#Delta = 0,3
+
+#Calculando o preço justo para a opção
+#0 + PrecoOpcao = 100*Delta - 105*Delta + (105 - 102)
+#0 + PrecoOpcao = 101*0,3 - 105*0,3 + 3
+#PrecoOpcao = 30,3 - 31,5 + 3 = 1,8
--- Lista1/Exercicio2.py
+++ Lista1/Exercicio2.py
@@ -1,1 +1,31 @@
 import numpy as np
+
+#Calculando a esperança
+#15/30 = 1/2 chances de perder o valor total
+#14/30 = 7/15 chances de perder metade do valor
+#1/30 chances de aumentar o total 50 vezes
+#E(X) = (1/2) * (1 - 1) + (7/15) * (1 - 0,5) + (1/30) * (1 + 50) - 1
+#E(X) = (1/2) * 0 + (7/15) * 0,5 + (1/30) * 51 - 1
+#E(X) = 0,933333333
+
+#Calculando a média temporal
+def proporcaoM(w):
+    return ((1 - w)**(15/30)) * ((1 - w/2)**(14/30)) * ((1 + 50*w)**(1/30)) - 1
+
+def proporcaoMSimplificado(w):
+    return ((1 - w)**(1/2)) * ((1 - w/2)**(7/15)) * ((1 + 50*w)**(1/30)) - 1
+
+print("RESULTADOS")
+
+print('1% ' + str(proporcaoM(0.01)))
+print('1% ' + str(proporcaoMSimplificado(0.01)) + '\n')
+print('2% ' + str(proporcaoM(0.02)))
+print('2% ' + str(proporcaoMSimplificado(0.02)) + '\n')
+print('2.5% ' + str(proporcaoM(0.025)))
+print('2.5% ' + str(proporcaoMSimplificado(0.025)) + '\n')
+print('3% ' + str(proporcaoM(0.03)))
+print('3% ' + str(proporcaoMSimplificado(0.03)) + '\n')
+print('4% ' + str(proporcaoM(0.04)))
+print('4% ' + str(proporcaoMSimplificado(0.04)) + '\n')
+print('5% ' + str(proporcaoM(0.05)))
+print('5% ' + str(proporcaoMSimplificado(0.05)) + '\n')
